3e6a25b931d87fb6554e13294506af61fd483907
Kristo Kakkum <krkadf@taltech.ee>
2025-02-05 17:05:16 +0200

    Kasutaja Aken

--- PrjConnector/My Project/Application.Designer.vb
+++ PrjConnector/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.PrjConnector.Form1
+            Me.MainForm = Global.PrjConnector.FormConverter
         End Sub
     End Class
 End Namespace
